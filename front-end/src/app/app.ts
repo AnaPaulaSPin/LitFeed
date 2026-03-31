fce47b717630e32ace91dc4bbf4ed15a1b30f010
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-03-31 11:13:13 -0300

    Footer do site
    Criando o componente Footer e implementando.

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html','./assets/css/owl.css',
+  imports: [RouterOutlet,Footer],
+  templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
