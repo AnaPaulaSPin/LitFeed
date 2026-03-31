f1c1017d43038e25c8c7fb87a1ba4a47ec533a34
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-03-31 10:37:32 -0300

    Pagina Inicial
    fazendo a tela inicial do site a partir de um modelo pronto :)

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.html','./assets/css/owl.css',
   styleUrl: './app.scss'
 })
 export class App {
