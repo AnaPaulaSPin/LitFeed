b0cfbb4e9cced04b4c65021c184f2129f6c89231
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-03-31 11:33:48 -0300

    Nav-bar Visitante
    Criando o componente da nav-bar para quando o usuário não cadastrado entrar no site.

---
+++ front-end/src/app/Nav-bar/visitante/visitante.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visitante',
+  imports: [],
+  templateUrl: './visitante.html',
+  styleUrl: './visitante.scss',
+})
+export class Visitante {}
--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
+import { Visitante } from './Nav-bar/visitante/visitante';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Footer],
+  imports: [RouterOutlet,Footer,Visitante],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
