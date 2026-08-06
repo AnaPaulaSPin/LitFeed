95a72914516c05bc1bf192ea5562dd6ee346911a
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-06 12:37:31 -0300

    Exibe dados do perfil
    Adicionadas as informações atuais do usuário na tela de edição de perfil, permitindo que os dados existentes sejam carregados previamente antes da alteração. A implementação facilita a visualização e edição das informações da conta pelo próprio usuário.

--- front-end/src/app/perfil/editar-perfil/editar-perfil.ts
+++ front-end/src/app/perfil/editar-perfil/editar-perfil.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-perfil',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './editar-perfil.html',
   styleUrl: './editar-perfil.scss',
 })
-export class EditarPerfil {}
+export class EditarPerfil {
+  user: any;
+
+  constructor() {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      this.user = JSON.parse(userData);
+    }
+  }
+
+}
