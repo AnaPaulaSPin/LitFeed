e6e9a034cf10fdeb596805c9a7a7bc0e4d6e9471
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-10 12:09:51 -0300

    Update Create_Table.sql
    Script da criação de tabelas.

--- LitFeed_Base/Create_Table.sql
+++ LitFeed_Base/Create_Table.sql
@@ -11,35 +11,45 @@ CREATE TABLE Usuario (
     biografia VARCHAR(255)
 );
 
-CREATE TABLE autor (
+CREATE TABLE Autor (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE editora (
+CREATE TABLE Editora (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE livro (
+CREATE TABLE Genero (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Livro (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(150) NOT NULL,
     isbn VARCHAR(20) UNIQUE,
     sinopse TEXT,
     capa VARCHAR(255),
     autor_id BIGINT NOT NULL,
+    genero_id BIGINT NOT NULL,
 
     CONSTRAINT id_autor
         FOREIGN KEY (autor_id)
-        REFERENCES autor(id)
-);
+        REFERENCES autor(id),
 
-CREATE TABLE edicao (
+    CONSTRAINT id_genero
+        FOREIGN KEY (genero_id)
+        REFERENCES Genero(id)
+);
+CREATE TABLE Edicao (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     livro_id BIGINT NOT NULL,
     editora_id BIGINT NOT NULL,
     capa VARCHAR(255),
     total_paginas INT NOT NULL,
+    ano INT NOT NULL,
 
     CONSTRAINT id_livro
         FOREIGN KEY (livro_id)
