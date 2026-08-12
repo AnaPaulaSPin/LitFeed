346d8dc176a4748ce606f3af6e86d2f4abea54e1
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-12 13:09:46 -0300

    Corrige atualização das publicações
    Corrigido o problema de atualização visual das publicações após o carregamento assíncrono dos dados. A requisição ao back-end retornava corretamente os registros e a lista era atualizada, porém o template não refletia as alterações imediatamente.
    
    Foi utilizado ChangeDetectorRef para solicitar manualmente uma nova detecção de mudanças após o recebimento das publicações. Com isso, os dados passaram a ser exibidos imediatamente na tela, sem necessidade de interação adicional com a página.

--- front-end/src/app/publicacao/publicacao/publicacao.ts
+++ front-end/src/app/publicacao/publicacao/publicacao.ts
@@ -1,30 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CardPublicacao } from '../card-publicacao/card-publicacao';
 import { Publicacao } from '../../models/publicacao';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { ServicePublicacao } from '../../Services/ServicePublicacao/service-publicacao';
 
 @Component({
   selector: 'app-publicacao',
-  imports: [CardPublicacao, NgFor],
+  imports: [CardPublicacao, NgFor, NgIf],
   templateUrl: './publicacao.html',
   styleUrl: './publicacao.scss',
 })
 export class PublicacaoModel {
 
   listaPublicacoes: Publicacao[] = [];
+  carregandoPublicacoes = true;
 
-  constructor(private publicacaoService: ServicePublicacao) {
+  constructor(private publicacaoService: ServicePublicacao,
+    private cdr: ChangeDetectorRef) {
     this.carregarPublicacoes();
   }
 
   carregarPublicacoes() {
+    this.carregandoPublicacoes = true;
+    
     this.publicacaoService.listarPublicacoes().subscribe({
       next: (publicacoes) => {
         this.listaPublicacoes = publicacoes;
+        this.carregandoPublicacoes = false;
+        this.cdr.detectChanges();
       },
+
       error: (erro) => {
-        console.error('Erro ao carregar publicações:', erro);
+        console.error('ERRO:', erro);
+
+        this.carregandoPublicacoes = false;
       }
     });
   }
