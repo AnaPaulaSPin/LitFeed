ec294c4f74057b690ef63271933e91a97b5494c8
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-15 12:27:11 -0300

    Implementa pesquisa de autores
    Implementada a área de pesquisa de autores, permitindo exibir todos os autores cadastrados e realizar a filtragem dinâmica conforme o usuário digita o nome.

--- front-end/src/app/Navegacao/Pesquisa/todos-autores/todos-autores.ts
+++ front-end/src/app/Navegacao/Pesquisa/todos-autores/todos-autores.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
+import { Autores } from '../../../Services/autores/autores';
+import { Autor } from './../../../models/autor';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { AutorCard } from '../../../Card/Autor/autor/autor';
 
 @Component({
   selector: 'app-todos-autores',
-  imports: [],
+  imports: [NgIf, NgFor, AutorCard, FormsModule],
   templateUrl: './todos-autores.html',
   styleUrl: './todos-autores.scss',
 })
-export class TodosAutores {}
+export class TodosAutores {
+  search: string = ''
+  autores: Autor[] = []
+  nenhumAutor = false
+  autoresFiltrados: Autor[] = []
+
+  constructor(private services: Autores,
+    private cdr: ChangeDetectorRef) {
+      this.carregarAutores()
+  }
+
+  carregarAutores(){
+    this.services.listarAutores().subscribe((autores) => {
+      this.autoresFiltrados = autores
+      this.autores = autores
+      this.cdr.detectChanges()
+    })
+  }
+
+  filtrarAutores() {
+    this.nenhumAutor = false;
+    if (this.search.trim() === '') {
+      this.autoresFiltrados = this.autores;
+      return;
+    }
+
+  this.autoresFiltrados = this.autores.filter((autor) => {
+    return autor.nome.toLowerCase().includes(this.search.toLowerCase());
+   });
+
+   if(this.autoresFiltrados.length === 0) {
+      this.nenhumAutor = true;
+    }
+ }
+}
