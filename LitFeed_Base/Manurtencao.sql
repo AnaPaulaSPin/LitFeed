96ac9d86a3f356cbb0cc583c83629ad73361c163
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-15 11:33:32 -0300

    Update Manurtencao.sql

--- LitFeed_Base/Manurtencao.sql
+++ LitFeed_Base/Manurtencao.sql
@@ -1,18 +1,60 @@
-UPDATE Livro SET capa = 'assets\\DataBase\\livros\\melhor-do-que-nos-filmes.jpg' WHERE titulo = 'Melhor do que nos filmes';
-UPDATE Livro SET capa = 'assets\\DataBase\\livros\\amor-por-engano.jpg' WHERE titulo = 'Amor por engano';
-UPDATE Livro SET capa = 'assets\\DataBase\\livros\\nao-e-como-nos-filmes.jpg' WHERE titulo = 'Não é como nos filmes (Melhor do que nos filmes – vol. 2)';
-UPDATE Livro SET capa = 'assets\\DataBase\\livros\\patinando-no-amor.jpg' WHERE titulo = 'Patinando no amor';
-UPDATE Livro SET capa = 'assets\\DataBase\\livros\\apostando-no-amor.jpg' WHERE titulo = 'Apostando no amor';
-UPDATE Edicao SET capa = 'assets\\DataBase\\livros\\melhor-do-que-nos-filmes.jpg' WHERE livro_id = (SELECT id FROM Livro WHERE titulo = 'Melhor do que nos filmes');
-UPDATE Edicao SET capa = 'assets\\DataBase\\livros\\amor-por-engano.jpg' WHERE livro_id = (SELECT id FROM Livro WHERE titulo = 'Amor por engano');
 
-SELECT * FROM livro;
+SELECT * FROM editora;
 SELECT * FROM genero;
 
 DELETE FROM genero WHERE id = 6;
+ALTER TABLE Autor
+ADD COLUMN foto VARCHAR(255);
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\Lynn-painter.jpg'
+WHERE nome = 'Lynn Painter';
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\sarah-adams.png'
+WHERE nome = 'Sarah Adams';
+
+-- ATUALIZAR FOTO DOS AUTORES
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\fiodor-dostoievski.jpg'
+WHERE nome = 'Fiódor Dostoiévski';
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\john-macarthur.jpg'
+WHERE nome = 'John MacArthur';
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\paul-washer.jpg'
+WHERE nome = 'Paul Washer';
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\machado-de-assis.jpg'
+WHERE nome = 'Machado de Assis';
+
+UPDATE Autor
+SET foto = 'assets\\DataBase\\Autores\\c-s-lewis.jpg'
+WHERE nome = 'C.S. Lewis';
 
 
-UPDATE Edicao SET capa = 'assets\\DataBase\\livros\\nao-e-como-nos-filmes.jpg' WHERE id = 3;
-UPDATE Edicao SET capa = 'assets\\DataBase\\livros\\patinando-no-amor.jpg' WHERE id = 4;
-UPDATE Edicao SET capa = 'assets\\DataBase\\livros\\apostando-no-amor.jpg' WHERE id = 5;
+-- ATUALIZAR FOTO DAS EDITORAS
+UPDATE Editora
+SET logo = 'assets\\DataBase\\Editoras\\intrinseca.jpg'
+WHERE nome = 'Intrínseca';
 
+UPDATE Editora
+SET logo = 'assets\\DataBase\\Editoras\\Editora-34.jpg'
+WHERE nome = 'Editora 34';
+
+UPDATE Editora
+SET logo = 'assets\\DataBase\\Editoras\\Thomas-Nelson-Brasil.jpg'
+WHERE nome = 'Thomas Nelson Brasil';
+
+UPDATE Editora
+SET logo = 'assets\\DataBase\\Editoras\\Editora-Fiel.jpg'
+WHERE nome = 'Editora Fiel';
+
+UPDATE Editora
+SET logo = 'assets\\DataBase\\Editoras\\Principis.jpg'
+WHERE nome = 'Principis';
