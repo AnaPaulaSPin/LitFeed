6955de4bcec088e9b6933517f2612274afb96cbe
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2026-08-29 13:08:25 -0300

    refactor: Card-Editora
    Atualiza a importação do modelo de Editora

--- front-end/src/app/Componentes/Card/Card-Editora/card-editora.ts
+++ front-end/src/app/Componentes/Card/Card-Editora/card-editora.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Editora } from '../../../models/editora';
+import { Editora } from '../../../Models/editora';
 
 @Component({
   selector: 'app-card-editora',
